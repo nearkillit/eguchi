70b0d5e8472a280d444f5cafd78d932a7ed6c1c0
nearkillit <82208192+nearkillit@users.noreply.github.com>
2021-04-09 18:01:05 +0900

    Create talktext.js

---
+++ talktext.js
@@ -0,0 +1,7 @@
+var textContents = ["こんにちは",
+                    "お名前をおしえてください input01",
+                    "output01 さんこんにちは",
+                    "今何歳ですか？ input02",
+                    "へぇ～",
+                    "output01 さんは output02 歳なんですね。"
+                    ]
